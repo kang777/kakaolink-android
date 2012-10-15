a2d67fb5c69872038ea431b779c5302de4565506
Nova <nova@kakao.com>
2012-10-15 20:23:17 +0900

    add StoryLink

--- example-link/src/com/example/link/MainActivity.java
+++ example-link/src/com/example/link/MainActivity.java
@@ -5,8 +5,12 @@ import java.util.Hashtable;
 import java.util.Map;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
 /**
@@ -22,7 +26,7 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main_activity);
 	}
 
 	/**
@@ -34,8 +38,10 @@ public class MainActivity extends Activity {
 		KakaoLink kakaoLink = KakaoLink.getLink(getApplicationContext());
 
 		// check, intent is available.
-		if (!kakaoLink.isAvailableIntent())
+		if (!kakaoLink.isAvailableIntent()) {
+			alert("Not installed KakaoTalk.");			
 			return;
+		}
 
 		/**
 		 * @param activity
@@ -83,8 +89,10 @@ public class MainActivity extends Activity {
 		KakaoLink kakaoLink = KakaoLink.getLink(getApplicationContext());
 		
 		// check, intent is available.
-		if(!kakaoLink.isAvailableIntent()) 
+		if(!kakaoLink.isAvailableIntent()) {
+			alert("Not installed KakaoTalk.");			
 			return;
+		}
 		
 		/**
 		 * @param activity
@@ -107,4 +115,96 @@ public class MainActivity extends Activity {
 				metaInfoArray);
 	}
 
+	
+	/**
+	 * Story Link
+	 * Send posting (text or url)
+	 * @throws NameNotFoundException 
+	 */
+	public void sendPostingLink(View v) throws NameNotFoundException {
+		Map<String, Object> urlInfoAndroid = new Hashtable<String, Object>(1);
+		urlInfoAndroid.put("title", "(광해) 실제 역사적 진실은?");
+		urlInfoAndroid.put("desc", "(광해 왕이 된 남자)의 역사성 부족을 논하다.");
+		urlInfoAndroid.put("imageurl", new String[] {"http://m1.daumcdn.net/photo-media/201209/27/ohmynews/R_430x0_20120927141307222.jpg"});
+		urlInfoAndroid.put("type", "article");
+		
+		// Recommended: Use application context for parameter.
+		StoryLink storyLink = StoryLink.getLink(getApplicationContext());
+
+		// check, intent is available.
+		if (!storyLink.isAvailableIntent()) {
+			alert("Not installed KakaoStory.");			
+			return;
+		}
+
+		/**
+		 * @param activity
+		 * @param post (message or url)
+		 * @param appId
+		 * @param appVer
+		 * @param appName
+		 * @param encoding
+		 * @param urlInfoArray
+		 */
+		storyLink.openKakaoLink(this, 
+				"http://m.media.daum.net/entertain/enews/view?newsid=20120927110708426",
+				getPackageName(), 
+				getPackageManager().getPackageInfo(getPackageName(), 0).versionName, 
+				"미디어디음",
+				encoding, 
+				urlInfoAndroid);
+	}
+	
+	/**
+	 * Story Link
+	 * Send posting (image)
+	 * @throws NameNotFoundException 
+	 */
+	public void sendPostingImage(View v) throws NameNotFoundException {
+		StoryLink storyLink = StoryLink.getLink(getApplicationContext());
+		
+		// check, intent is available.
+		if (!storyLink.isAvailableIntent()) {
+			alert("Not installed KakaoStory.");
+			return;
+		}
+		
+		Intent intent = new Intent(Intent.ACTION_PICK);
+		intent.setType("image/*");
+		intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		startActivityForResult(intent, 0); 
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (resultCode != RESULT_OK) {
+			return;
+		}
+		
+		Cursor c = getContentResolver().query(intent.getData(), null, null, null, null);
+		c.moveToNext();
+		String path = c.getString(c.getColumnIndex(MediaStore.MediaColumns.DATA));
+		c.close();
+		
+		StoryLink storyLink = StoryLink.getLink(getApplicationContext());
+
+		// check, intent is available.
+		if (!storyLink.isAvailableIntent()) {
+			alert("Not installed KakaoStory.");
+			return;
+		}
+		
+		storyLink.openStoryLinkImageApp(this, path);
+
+	}
+	
+	private void alert(String message) {
+		new AlertDialog.Builder(this)
+			.setIcon(android.R.drawable.ic_dialog_alert)
+			.setTitle(R.string.app_name)
+			.setMessage(message)
+			.setPositiveButton(android.R.string.ok, null)
+			.create().show();
+	}
+	
 }
